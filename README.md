528aaee96c3c7a5e77489471e1507bf1c80e86ec
markgrig <110450201+markgrig@users.noreply.github.com>
2022-08-30 15:24:03 +0300

    new_vers 30/08/20022

--- README.md
+++
@@ -1,4 +0,0 @@
-<h2>
-Cписок задач по программированию.
-</h2>
-<h3>https://markgrig.github.io/list_of_tasks/ </h3>
--- js/index.js
+++ js/index.js
@@ -1,39 +1,113 @@
-//создание объектов с использованием ООП, функция
-function Animal(name){
-    //this метод создающий ключи
-    this.name = name;
-    this.getName = function() {
-        return name
-    }
+const navigation = document.querySelectorAll('.navigator a')
+
+navigation.forEach( anchor => {
+        anchor.addEventListener(  'click' , ( event ) => {
+            navigation.forEach( element => {
+                element.style.boxShadow = "none"
+            })
+            const { target } = event
+            target.style.boxShadow = "inset 0 -6px 0 rgba(189, 35, 35, 0.842)"
+        }  
+    ) 
+});
+
+document.addEventListener( 'keydown' , ( event ) => {
+    //console.log(event);
+    const { key } =  event
+    const { altKey } = event
+    const deleteTask = document.querySelectorAll(`li`)
+    deleteTask.forEach( (element, index )=> {
+     
+        if ( element && altKey && ( index + 1 === Number(key) ) ) {
+            const confrimDelete = confirm(`Вы действительно хотите удалить задачу ${key} ?`)
+            if ( confrimDelete) {
+               
+                while ( element.nextElementSibling ) {
+                    element.nextElementSibling.remove()
+                }
+                element.remove()
+            }
+        }
+    });
+})
+
+const creatToolTip = (text) => {
+    const toolTip = document.createElement('span')
+    toolTip.textContent = text
+    toolTip.className = 'toolTip'
+    return toolTip
 }
 
-//создаёт объект 
-console.log ( "new создаёт объект", new Animal("кися") )
-// не создаёт объект
-console.log ( "без new объект не создаётся", Animal("кися") )
+const buttonForDelete = document.querySelector('ol').querySelectorAll('#buttonForDelete')
 
-console.log(  "свойство name ->", new Animal("кися").name);
-console.log(  "функция геттер ->",new Animal("кися").getName());   
 
-class Animals {
-    //конструктор - основная цель, которого инициализация параметров вначале 
-    constructor(name) {
-        this.name = name
-    }
-    getName() {
-        return this.name
-    }
-    setName(new_name){
-        this.name = new_name
-    }
+buttonForDelete.forEach( (element, index) => {
+    element.addEventListener( 'mouseover', (event) => {
+        const { target } = event 
+
+        const toolTipeHTML = creatToolTip( `Нажмите, чтобы удалить ${index+1} задачу` )
+        target.insertAdjacentElement('afterEnd',toolTipeHTML)
+    
+    })    
+});
+
+buttonForDelete.forEach( (element, index) => {
+    element.addEventListener( 'mouseout', (event) => {
+        const { target } = event 
+        target.nextElementSibling.remove()
+    })    
+});
+
+//ещё есть mousemove
+
+document.addEventListener( 'contextmenu' , ( event ) => {
+    //console.log(event);
+    //event.preventDefault()
+
+})
+
+
+//change (после ввода), input (сразу!)
+
+const checkTextValidation = ( text ) => {
+     if ( !text ) {
+        return false 
+     } 
+     else {
+        return true 
+     }
 }
 
-//создаёт объект 
-console.log ( "создание объекта с помощью class", new Animals("собака") )
-console.log(  "свойство name ->", new Animal("собака").name);
-console.log(  "функция геттер ->",new Animal("собака").getName());  
+const textForAdd = document.querySelector('.textForm')
 
-const bird = new Animals("воробей")
-console.log("создание воробья" , bird);
-bird.setName("соловей")
-console.log("измения птицы функцией сеттер" , bird );
+textForAdd.addEventListener( 'input' , (event) => {
+   
+    const { target } = event
+    const { value } = target
+    
+    const messageBoxDOM = document.querySelector('.messageAboutError')
+    if ( !checkTextValidation(value)) {
+        const newMassageBox = document.createElement('span')
+        newMassageBox.className = 'messageAboutError'
+        newMassageBox.textContent = 'Пустое поле для добавления задачи!'
+        textForAdd.insertAdjacentElement('afterEnd',newMassageBox)
+    } 
+    else if ( checkTextValidation(value) && messageBoxDOM ){
+        messageBoxDOM.remove()
+    }
+})
+
+//submit    
+const textForm = document.querySelector('.textForm')
+textForm.addEventListener( 'submit' , ( event ) => {
+        //console.log(event)
+        event.preventDefault()
+        const { target } = event
+        if (target.nameTask.value) {
+            alert(`Вы создали задачу ${target.nameTask.value.trim()}` )
+        }
+})
+
+
+
+    
