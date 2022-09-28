c8c873c33ae907ef0402d6017c7e6a1cf219658c
markgrig <110450201+markgrig@users.noreply.github.com>
2022-09-28 17:54:33 +0300

    27/09/20022 new-relize

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,92 @@ import "../img/background4.jpg"
 const bodyElement = document.querySelector('body') 
 bodyElement.style.backgroundImage = `url(./img/background1.jpg)`
 
+//localStorage.clear()
 //навигация по сайту
-const navigation = document.querySelector('.navigator')
-let listOfTask = document.querySelector('.list') 
+const navigation = bodyElement.querySelector('.navigator')
+let listOfTask = bodyElement.querySelector('.list') 
+
+//задачи добавляемые в список с использованием JS
+const tasksToday = JSON.parse( localStorage.getItem("tasksToday") ) ||
+{
+    array: 
+    [
+        {
+            text: "Повторить пройденный материал"
+        },
+        {
+            text: "Cделать Дз к уроку по JavaScript"
+        },
+        {
+            text: 'Посмотреть новый урок по JavaScript',
+        },
+        {
+    
+            text: 'Выполнить тест после урока',
+        },
+        {
+            text: 'Выполнить ДЗ после урока',
+        },
+    ],
+    category: "today"
+}
+
+const tasksWeek = JSON.parse( localStorage.getItem("tasksWeek") ) ||
+ {
+    array: 
+    [
+        {
+            text: "Начать учить какой-то модный фреймворк"
+        },
+        {
+            text: "Сделать сервре Node.js общедсотупным"
+        },
+        {
+            text: 'Начать новый проект',
+        },
+    ],
+    category: "week"
+}
+
+
+
+const tasksFuture = JSON.parse( localStorage.getItem("tasksFuture") ) ||
+{
+    array: 
+    [
+        {
+            text: "Выучить какой-то модный фреймворк"
+        },
+        {
+            text: "Оптимизировать свои самые первые проекты"
+        },
+    ]
+    ,
+    category: "future"
+}
+
+const renderTasks = (tasks, category) => {
+
+    const listCategory= bodyElement.querySelector('#'+ category)  
+            
+    tasks.forEach( ( el, i ) => {
+        const div = document.createElement('div')
+        const HTMLforApp = 
+        `
+        <li id = "${i}" >  ${el.text} </li> 
+        <button  class="button"  type = 'submit' id = "buttonForDelete"> Удалить  </button>
+        `
+        div.innerHTML = HTMLforApp
+
+        listCategory.append(div)
+        
+    })
+
+} 
+
+renderTasks( tasksToday.array , "today")
+renderTasks( tasksWeek.array , "week")
+renderTasks( tasksFuture.array , "future")
 
 navigation.addEventListener(  'click' , ( event ) => {
 
@@ -41,36 +124,13 @@ navigation.addEventListener(  'click' , ( event ) => {
         bodyElement.querySelectorAll('.list').forEach(element => {
             element.classList.add('selectorHidden')
         });
-        listOfTask = bodyElement.querySelector( '#' + target.getAttribute('data-name'))
+
+        const category = target.getAttribute('data-name')
+        listOfTask = bodyElement.querySelector( '#' + category)
         listOfTask.classList.remove('selectorHidden')
+
     }
 })                       
-
-
-//задачи добавляемые в список с использованием JS
-const tasks = [
-    {
-        text: 'Посмотреть новый урок по JavaScript',
-    },
-    {
-  
-        text: 'Выполнить тест после урока',
-    },
-    {
-        text: 'Выполнить ДЗ после урока',
-    },
-]
-
-tasks.forEach( ( el) => {
-    const div = document.createElement('div')
-    const HTMLforApp = 
-    `
-    <li>  ${el.text} </li> 
-    <button  class="button"  type = 'submit' id = "buttonForDelete"> Удалить  </button>
-    `
-    div.innerHTML = HTMLforApp
-    listOfTask.append(div)
-})
 
 let indexBackgroundImage = 1
 
@@ -91,7 +151,7 @@ document.addEventListener( 'keydown' , ( event ) => {
             const blackBackground = document.querySelector("#black-background")
            
 
-            modalWindow.firstChild.nextElementSibling.textContent = `Вы действительно хотите удалить задачу под номером ${key} ?`
+            modalWindow.firstChild.nextElementSibling.textContent = `Вы xотите удалить задачу под номером ${key} ?`
             
             modalWindow.classList.add( 'modalOverlay')
             modalWindow.classList.remove( 'selectorHidden')
@@ -107,6 +167,16 @@ document.addEventListener( 'keydown' , ( event ) => {
                     element.nextElementSibling.remove()
                 }
                 element.remove()
+                console.log(element);
+                
+                const category = bodyElement.querySelector('.titleName').getAttribute('data-name') 
+                  
+                if ( tasksToday.category == category ) { 
+                    tasksToday.array.splice(index, 1 );
+                    console.log(tasksToday);
+                    localStorage.setItem("tasksToday", JSON.stringify( tasksToday ))
+                }
+                
                 modalWindow.classList.add( 'selectorHidden')
                 blackBackground.classList.add( 'selectorHidden')
             
@@ -124,9 +194,11 @@ document.addEventListener( 'keydown' , ( event ) => {
         indexBackgroundImage += 1
         if ( indexBackgroundImage === 5 ) {
             indexBackgroundImage = 1
+        } 
+        if ( indexBackgroundImage === 0 ) {
+            indexBackgroundImage = 4
         }
 
-        const bodyElement = document.querySelector('body')
         bodyElement.style.backgroundImage = `url(./img/background${indexBackgroundImage}.jpg)`
         
     }
@@ -145,8 +217,6 @@ let buttonForDelete = document.querySelector('ol').querySelectorAll('#buttonForD
 
 bodyElement.addEventListener( 'mouseover', (event) => {
     const { target } = event 
-
-    
     //события наведения на подсказку 
     if (target.closest('#buttonForDelete')) {
         console.log(target.previousElementSibling);
@@ -155,12 +225,17 @@ bodyElement.addEventListener( 'mouseover', (event) => {
     }
 })
 
-
 bodyElement.addEventListener( 'mouseout', (event) => {
     const { target } = event 
+    // код для удаления подсказок
+    if (target.closest('#buttonForDelete')) {
+        target.nextElementSibling.remove()
+        }
+})
 
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-         //события появления модального окна
+bodyElement.addEventListener( 'touchend', (event) => {
+    const { target } = event  
+    //события появления модального окна
     if (target.closest('#buttonForDelete')) {
         const modalWindow = document.querySelector("#deleteModal")
         const blackBackground = document.querySelector("#black-background")
@@ -176,24 +251,51 @@ bodyElement.addEventListener( 'mouseout', (event) => {
         const buttonYes = document.querySelector(".buttonYes")
         const buttonNo  = document.querySelector(".buttonNo")
 
-        buttonYes.addEventListener( 'click',  ( event ) => {
+        buttonYes.addEventListener( 'touchend',  ( event ) => {
             target.closest('div').remove()
-            modalWindow.classList.add( 'selectorHidden')
-            blackBackground.classList.add( 'selectorHidden')
-        })
-        buttonNo.addEventListener( 'click',  ( event ) => {
-            modalWindow.classList.add( 'selectorHidden')
-            blackBackground.classList.add( 'selectorHidden')
-        })
-    }
-      } else {
-        // код для обычных устройств
-        if (target.closest('#buttonForDelete')) {
-            target.nextElementSibling.remove()
-            }
-    }
-   
 
+            const indexDelete =  target.previousElementSibling.id
+            const category = bodyElement.querySelector('.titleName').getAttribute('data-name') 
+
+            modalWindow.classList.add( 'selectorHidden')
+            blackBackground.classList.add( 'selectorHidden')
+
+            if ( tasksToday.category == category ) { 
+                tasksToday.array.splice(indexDelete, 1 );
+                localStorage.setItem("tasksToday", JSON.stringify( tasksToday ))
+            }
+
+            if ( tasksWeek.category == category ) { 
+                tasksWeek.array.splice(indexDelete, 1 );
+                localStorage.setItem("tasksWeek", JSON.stringify( tasksWeek ))
+            }
+
+            if ( tasksFuture.category == category ) { 
+                tasksFuture.array.splice(indexDelete, 1 );
+                localStorage.setItem("tasksFuture", JSON.stringify( tasksFuture ))
+            }
+
+        })
+        buttonNo.addEventListener( 'touchend',  ( event ) => {
+            modalWindow.classList.add( 'selectorHidden')
+            blackBackground.classList.add( 'selectorHidden')
+        })
+    }
+    if (target.closest('#last-background')) {
+        indexBackgroundImage -= 1 
+        if ( indexBackgroundImage === 0 ) {
+            indexBackgroundImage = 4
+        }
+
+        bodyElement.style.backgroundImage = `url(./img/background${indexBackgroundImage}.jpg)`
+    }
+    if (target.closest('#next-background')) {
+        indexBackgroundImage += 1
+        if ( indexBackgroundImage === 5 ) {
+            indexBackgroundImage = 1
+        } 
+        bodyElement.style.backgroundImage = `url(./img/background${indexBackgroundImage}.jpg)`
+    }
 }) 
 
 
@@ -202,6 +304,7 @@ bodyElement.addEventListener( 'click' , ( event ) => {
     const { target } = event 
     
     //события появления модального окна
+    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )) {
     if (target.closest('#buttonForDelete')) {
         const modalWindow = document.querySelector("#deleteModal")
         const blackBackground = document.querySelector("#black-background")
@@ -215,15 +318,63 @@ bodyElement.addEventListener( 'click' , ( event ) => {
         const buttonYes = document.querySelector(".buttonYes")
         const buttonNo  = document.querySelector(".buttonNo")
 
+        
         buttonYes.addEventListener( 'click',  ( event ) => {
+            const indexDelete =  target.previousElementSibling.id
+            
+                            
+            const category = bodyElement.querySelector('.titleName').getAttribute('data-name') 
+                  
+            if ( tasksToday.category == category ) { 
+                tasksToday.array.splice(indexDelete, 1 );
+                localStorage.setItem("tasksToday", JSON.stringify( tasksToday ))
+            }
+
+            if ( tasksWeek.category == category ) { 
+                tasksWeek.array.splice(indexDelete, 1 );
+                localStorage.setItem("tasksWeek", JSON.stringify( tasksWeek ))
+            }
+
+            if ( tasksFuture.category == category ) { 
+                tasksFuture.array.splice(indexDelete, 1 );
+                localStorage.setItem("tasksFuture", JSON.stringify( tasksFuture ))
+            }
+
+
             target.closest('div').remove()
             modalWindow.classList.add( 'selectorHidden')
             blackBackground.classList.add( 'selectorHidden')
+
         })
         buttonNo.addEventListener( 'click',  ( event ) => {
             modalWindow.classList.add( 'selectorHidden')
             blackBackground.classList.add( 'selectorHidden')
         })
+    }
+    if (target.closest('#last-background')) {
+        indexBackgroundImage -= 1 
+        if ( indexBackgroundImage === 0 ) {
+            indexBackgroundImage = 4
+        }
+
+        bodyElement.style.backgroundImage = `url(./img/background${indexBackgroundImage}.jpg)`
+    }
+    if (target.closest('#next-background')) {
+        indexBackgroundImage += 1
+        if ( indexBackgroundImage === 5 ) {
+            indexBackgroundImage = 1
+        } 
+        bodyElement.style.backgroundImage = `url(./img/background${indexBackgroundImage}.jpg)`
+    }
+    }
+    else{
+        const menuElement = bodyElement.querySelector("#menu")
+        if (target.closest('.textForAdd')) {
+            menuElement.classList.add( 'selectorHidden')
+        }
+        else {
+            menuElement.classList.remove( 'selectorHidden')
+        }
     }
 })   
 
@@ -231,7 +382,12 @@ bodyElement.addEventListener( 'click' , ( event ) => {
 const checkTextValidation = ( text ) => {
     const errorArray = []
     if ( !text ) {
-        errorArray.push('Пустое поле для добавления задачи!') 
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            errorArray.push('Пустое поле!') 
+        }
+        else {
+            errorArray.push('Пустое поле для добавления задачи!') 
+        }
      } 
      else {
         const arrayTasks = listOfTask.querySelectorAll(`li`)
@@ -254,7 +410,7 @@ textForAdd.addEventListener( 'input' , (event) => {
     
     const messageBoxDOM = document.querySelector('.messageAboutError')
     const errorArray = checkTextValidation(value)
-   
+    
     if ( errorArray[0] ) {
 
         if ( messageBoxDOM ){
@@ -271,6 +427,7 @@ textForAdd.addEventListener( 'input' , (event) => {
     else if ( checkTextValidation(value) && messageBoxDOM ){
         messageBoxDOM.remove()
     }
+
 })
   
 const textForm = document.querySelector('.textForm')
@@ -285,12 +442,26 @@ textForm.addEventListener( 'submit' , ( event ) => {
             const newTask = {
                 text: `${target.nameTask.value.trim()}`,
             }
-            tasks.push(newTask)
+
+            const category = bodyElement.querySelector('.titleName').getAttribute('data-name') 
+                  
+            if ( tasksToday.category == category ) { 
+                tasksToday.array.push(newTask)
+                localStorage.setItem("tasksToday", JSON.stringify( tasksToday ))
+            }
+            if ( tasksWeek.category == category ) {  
+                tasksWeek.array.push(newTask) 
+                localStorage.setItem("tasksWeek", JSON.stringify( tasksWeek ))
+            }
+            if ( tasksFuture.category == category ) {  
+                tasksFuture .array.push(newTask)
+                localStorage.setItem("tasksFuture", JSON.stringify( tasksFuture ))
+            }
             
             const div = document.createElement('div')
             const HTMLforAdd = 
             `
-            <li data-number-task="${listOfTask.querySelectorAll('div').length + 1}">  ${newTask .text} </li> 
+            <li id="${listOfTask.querySelectorAll('div').length }">  ${newTask .text} </li> 
             <button  class="button"  type = 'submit' id = "buttonForDelete"> Удалить  </button>
             `
             div.innerHTML = HTMLforAdd
